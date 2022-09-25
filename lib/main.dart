c3a2a8b66ce3daac068eedef68ac9ba8521933cc
Revangga Aji Pratama <revanggaajip2@gmail.com>
2022-09-25 15:33:43 +0700

    Belajar menggunakan ListView.builder

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(AplikasiKu());
 
 class AplikasiKu extends StatelessWidget {
-  List<Container> dataku = [
-    Container(width: 300, height: 300, color: Colors.red),
-    Container(width: 300, height: 300, color: Colors.blue),
-    Container(width: 300, height: 300, color: Colors.yellow),
-    Container(width: 300, height: 300, color: Colors.orange),
+  final List<Color> dataWarna = [
+    Colors.cyan,
+    Colors.yellow,
+    Colors.blueAccent,
+    Colors.redAccent
   ];
 
   @override
@@ -15,14 +15,14 @@ class AplikasiKu extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text("Latihan List View")),
-        body: ListView(
-          // ignore: sort_child_properties_last
-          children: dataku,
-          //Untuk mengubah arah scroll
-          scrollDirection: Axis.vertical,
-        ),
-      ),
+          appBar: AppBar(title: const Text("Latihan List View")),
+          body: ListView.builder(
+            itemCount: dataWarna.length,
+            itemBuilder: (context, index) {
+              return Container(
+                  width: 300, height: 300, color: dataWarna[index]);
+            },
+          )),
     );
   }
 }
