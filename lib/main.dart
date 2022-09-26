288f71bef677a297fd301a22b04cbef5f0919ae4
Revangga Aji Pratama <revanggaajip2@gmail.com>
2022-09-26 09:27:35 +0700

    Belajar Menggunakan List Generate

--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,40 @@ import 'package:flutter/material.dart';
 void main() => runApp(AplikasiKu());
 
 class AplikasiKu extends StatelessWidget {
-  final List<Color> dataWarna = [
-    Colors.cyan,
-    Colors.yellow,
-    Colors.blueAccent,
-    Colors.redAccent
-  ];
+  // final List<Color> dataWarna = [
+  //   Colors.cyan,
+  //   Colors.yellow,
+  //   Colors.blueAccent,
+  //   Colors.redAccent
+  // ];
+
+  List<Widget> angka = List.generate(
+      100,
+      (index) => Text(
+            "${index + 1}",
+            style: TextStyle(fontSize: 20 + double.parse(index.toString())),
+          ));
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
           appBar: AppBar(title: const Text("Latihan List View")),
-          body: ListView.separated(
-            separatorBuilder: (context, index) {
-              return Divider();
-            },
-            itemCount: dataWarna.length,
-            itemBuilder: (context, index) {
-              return Container(
-                  width: 300, height: 300, color: dataWarna[index]);
-            },
-          )),
-    );
+          body: ListView(
+            children: angka,
+          ),
+          // Menggunakan color
+          // body: ListView.separated(
+          //   separatorBuilder: (context, index) {
+          //     return const Divider();
+          //   },
+          //   itemCount: dataWarna.length,
+          //   itemBuilder: (context, index) {
+          //     return Container(
+          //         width: 300, height: 300, color: dataWarna[index]);
+          //   },
+          // )),
+        ));
   }
 }
